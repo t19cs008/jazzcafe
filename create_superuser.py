06b610cfc678d033cf73809defd51068bda8afb6
t19cs008 <92138482+t19cs008@users.noreply.github.com>
2024-11-13 12:59:44 +0900

    html内のtableにtable_responsiveを追加 (#3)
    * スーパーユーザの自動生成を追加
    
    * スーパーユーザの自動生成のコードの修正
    
    * tableにtable-responsiveを追加
    
    ---------
    
    Co-authored-by: g23tk004 <g23tk004@aw01.kki.yamanashi.ac.jp>

--- create_superuser.py
+++ create_superuser.py
@@ -5,7 +5,7 @@ def create_superuser():
     user_name = os.environ.get("DJANGO_SUPERUSER_USERNAME")
     email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
     password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-    
+
     if user_name is None or email is None or password is None:
         return
 
