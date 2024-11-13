f6a307cb58c838f40202704bb629a87680b49ade
g23tk004 <g23tk004@aw01.kki.yamanashi.ac.jp>
2024-11-13 12:21:36 +0900

    スーパーユーザの自動生成のコードの修正

--- create_superuser.py
+++ create_superuser.py
@@ -2,16 +2,13 @@ import subprocess
 import os
 
 def create_superuser():
-    user_name = os.environ.get("SUPERUSER_USERNAME")
-    email = os.environ.get("SUPERUSER_EMAIL")
-    password = os.environ.get("SUPERUSER_PASSWORD")
+    user_name = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
     if user_name is None or email is None or password is None:
         return
 
-    subprocess.run(["python", "manage.py", "createsuperuser",
-                    "--username", user_name,
-                    "--email", email,
-                    "--password", password])
+    subprocess.run(["python", "manage.py", "createsuperuser", "--noinput"])
     
 create_superuser()
